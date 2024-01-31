a6c9ba01f0158b1cd547b595cbde130f8ad109b7
icornelis90 <isolde.cornelis@ilvo.vlaanderen.be>
2024-01-31 14:59:56 +0100

    Update Step6_Script_microDecon.R
    Update the Script to remove samples with 0 reads

--- 12S/Step6_Script_microDecon.R
+++ 12S/Step6_Script_microDecon.R
@@ -15,28 +15,32 @@ library(here)
 library(devtools)
 devtools::install_github("donaldtmcknight/microDecon")
 
+# make paths
+proj.path <- here("/home/genomics/icornelis/02_ZEROimpact/01_12S/NJ2021/MiFish-UE_run2")
 
 # upload the dataset
+env <- read.csv(paste0(proj.path,"/Step5_Statistics/environmental_data.csv"),  header=TRUE, sep=";")
 table_raw <- readxl::read_excel(paste0(proj.path, "/MiFish_UE-S_concatenated/results_v2/table_unrarefied_raw_Full_TaxAss_WoRMS.xlsx"))
-table_raw_noT <- as.data.frame(table_raw[,1:(ncol(table_raw)-11)])
-table_raw_noT$ASV <- table_raw$ASV
-table_raw_noT <- table_raw_noT %>% relocate(ASV, .before = '1_1_DYF_NJ2021_S1')
-table_raw_noT$Taxa <- table_raw$Species
 
+# Remove the taxonomic assignment
+table_raw_noT <- as.data.frame(table_raw[,1:(ncol(table_raw)-11)])
+
+#Reorder the dataframe: ASV_names (always first column), negative controls, true samples, Taxa (always last column)
 table_raw_neg <- table_raw_noT[, grepl("neg", colnames(table_raw_noT))]
 table_raw_sample <- table_raw_noT[, !colnames(table_raw_noT) %in% c(colnames(table_raw_neg))]
+table_raw_noT_order <- cbind(table_raw$ASV, table_raw_neg, table_raw_sample, table_raw$Species)
+colnames(table_raw_noT_order)[c(1,ncol(table_raw_noT_order))] <- c("ASV", "Taxa")
 
-table_raw_noT_order <- cbind(table_raw$ASV, table_raw_neg, table_raw_sample)
-
+#Select samples from the DYFS-campaign in September
 DYFS_samples <- c(env$Niskin.sample[which(env$Zones == "Coastal" & !env$Location == "ft230")])
 DYFS <- c(DYFS_samples, env$Niskin.sample[which(env$Location %in% c("neg_Coastal", "neg_PCR"))], 
           "filter_5", "filter_6", "neg_DNA_3", "ASV", "Taxa")
 
 table_DYFS <- table_raw_noT_order[, grepl(paste(DYFS, collapse = "|"), 
                                           colnames(table_raw_noT_order))]
-remove_sample <- c(colnames(table_DYFS[, colSums(table_DYFS[,2:(ncol(table_DYFS)-1) == 0])]))
-table_DYFS <- table_DYFS[, !colnames(table_DYFS) == "neg_PCR_2_S2"]
-table_DYFS_Field <- table_DYFS[, !grepl("PCR|filter", colnames(table_DYFS))]
+table_DYFS_num <- table_DYFS[,2:(ncol(table_DYFS)-1)]
+remove_DYFS <- c(colnames(table_DYFS_num[colSums(table_DYFS_num) == 0]))
+table_DYFS <- table_DYFS[, !colnames(table_DYFS) %in% remove_DYFS]
 neg_DYFS <- length(c(colnames(table_DYFS[, grepl("neg", colnames(table_DYFS))])))
 numb_DYFS <- c(rep(9, times = length(DYFS_samples)/3))
 
@@ -44,7 +48,16 @@ decontaminated_DYFS <- decon(data = table_DYFS, numb.blanks=neg_DYFS, numb.ind=n
                              runs=2,thresh = 0.7,prop.thresh = 0.00005,regression=0,low.threshold=40,up.threshold=400)
 
 decon_DYFS <- as.data.frame(decontaminated_DYFS$decon.table)
-contaminant <- as.data.frame(decontaminated_DYFS$OTUs.removed)
+contaminant_DYFS <- as.data.frame(decontaminated_DYFS$OTUs.removed)
+
+table_DYFS_Field <- table_DYFS[,!grepl("filter|DNA|PCR", colnames(table_DYFS))]
+neg_DYFS_Field <- length(c(colnames(table_DYFS_Field[, grepl("neg", colnames(table_DYFS_Field))])))
+
+decontaminated_DYFS_Field <- decon(data = table_DYFS_Field, numb.blanks=neg_DYFS_Field, numb.ind=numb_DYFS, taxa=T,
+                             runs=2,thresh = 0.7,prop.thresh = 0.00005,regression=0,low.threshold=40,up.threshold=400)
+
+decon_DYFS_Field <- as.data.frame(decontaminated_DYFS$decon.table)
+contaminant_DYFS_Field <- as.data.frame(decontaminated_DYFS$OTUs.removed)
 
 Geo_samples <- c(env$Niskin.sample[which(env$Zones %in% c("zone1", "zone2") | env$Location == "ft230")])
 Geo <- c(Geo_samples, env$Niskin.sample[which(env$Location %in% c("neg_inside_OWF", "neg_outside_OWF", "neg_PCR"))], 
@@ -53,18 +66,29 @@ Geo <- c(Geo_samples, env$Niskin.sample[which(env$Location %in% c("neg_inside_OW
 
 table_Geo <- table_raw_noT_order[, grepl(paste(Geo, collapse = "|"), 
                                           colnames(table_raw_noT_order))]
-remove_sample <- c(colnames(table_Geo[, colSums(table_Geo[,2:(ncol(table_Geo)-1) == 0])]))
-table_Geo <- table_Geo[, !colnames(table_Geo) == "neg_PCR_2_S2"]
+table_Geo <- table_Geo[, !grepl("_bis", colnames(table_Geo))]
+table_Geo_num <- table_Geo[,2:(ncol(table_Geo)-1)]
+remove_Geo <- c(colnames(table_Geo_num[colSums(table_Geo_num) == 0]))
+table_Geo <- table_Geo[, !colnames(table_Geo) %in% remove_Geo]
 table_Geo <- table_Geo %>% relocate("ftTrack5_4_S2", .before = 'ftTrack5_4_S3')
 table_Geo <- table_Geo %>% relocate("ftWT1bis_4_S2", .before = 'ftWT1bis_4_S3')
 neg_Geo <- length(c(colnames(table_Geo[, grepl("neg", colnames(table_Geo))])))
-numb_Geo <- c(15,15,15,15,15,15,14,15,15,15,15,15,15,15,15,15,15,15,15)
+numb_Geo <- c(9,15,15,15,15,15,14,15,15,15,15,15,15,15,15,15,15,15,15)
 
 decontaminated_Geo <- decon(data = table_Geo, numb.blanks=neg_Geo, numb.ind=numb_Geo, taxa=T,
                              runs=2,thresh = 0.7,prop.thresh = 0.00005,regression=0,low.threshold=40,up.threshold=400)
 
 decon_Geo <- as.data.frame(decontaminated_Geo$decon.table)
-contaminant <- as.data.frame(decontaminated_Geo$OTUs.removed)
+contaminant_Geo <- as.data.frame(decontaminated_Geo$OTUs.removed)
+
+table_Geo_Field <- table_Geo[,!grepl("filter|DNA|PCR", colnames(table_Geo))]
+neg_Geo_Field <- length(c(colnames(table_Geo_Field[, grepl("neg", colnames(table_Geo_Field))])))
+
+decontaminated_Geo_Field <- decon(data = table_Geo_Field, numb.blanks=neg_Geo_Field, numb.ind=numb_Geo, taxa=T,
+                                   runs=2,thresh = 0.7,prop.thresh = 0.00005,regression=0,low.threshold=40,up.threshold=400)
+
+decon_Geo_Field <- as.data.frame(decontaminated_Geo$decon.table)
+contaminant_Geo_Field <- as.data.frame(decontaminated_Geo$OTUs.removed)
 
 table_clean <- merge(decon_DYFS, decon_Geo, by = 1, all = T)
 table_clean <- table_clean[, !grepl("Taxa|blank", colnames(table_clean))]
