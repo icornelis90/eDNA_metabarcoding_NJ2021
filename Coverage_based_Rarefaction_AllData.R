67d63854baf1563407bf9f3b0529857f01d350c7
icornelis90 <isolde.cornelis@outlook.be>
2024-02-27 15:28:46 +0100

    Update Coverage_based_Rarefaction_AllData.R
    Add Statistical analysis

--- Coverage_based_Rarefaction_AllData.R
+++ Coverage_based_Rarefaction_AllData.R
@@ -87,6 +87,7 @@ ps_unrarefied_12S <- phyloseq(otu_table(ps_eDNA_12S, taxa_are_rows = TRUE),
 
 ##Calculate the Coverage
 Coverage_eDNA_12S <- phyloseq_coverage(physeq = ps_unrarefied_12S)
+Coverage_eDNA_12S_correct_singletons <- phyloseq_coverage(physeq = ps_unrarefied_12S, correct_singletons = TRUE)
 
 ##Rarefy the data based on a coverage just below the minimum coverage using the function phyloseq_coverage_raref,
 ##(using the minimum coverage will remove the sample from the rarefied dataset)
@@ -110,6 +111,7 @@ ps_unrarefied_COI <- phyloseq(otu_table(ps_eDNA_COI, taxa_are_rows = TRUE),
 
 ##Calculate the Coverage
 Coverage_eDNA_COI <- phyloseq_coverage(physeq = ps_unrarefied_COI)
+Coverage_eDNA_COI_correct_singletons <- phyloseq_coverage(physeq = ps_unrarefied_COI, correct_singletons = TRUE)
 
 ##Rarefy the data based on a minimum coverage using the function phyloseq_coverage_raref,
 ##(using the minimum coverage that will not generate an error)
@@ -203,6 +205,7 @@ ps_morph_Fish_phylo <- phyloseq(otu_table(ps_morph_Fish, taxa_are_rows = TRUE))#
 
 ##Calculate the Coverage
 Coverage_morph_Fish <- phyloseq_coverage(physeq = ps_morph_Fish_phylo)
+Coverage_morph_Fish_correct_singletons <- phyloseq_coverage(physeq = ps_morph_Fish_phylo, correct_singletons = TRUE)
 
 ##Rarefy the data based on a minimum coverage using the function phyloseq_coverage_raref,
 ##(using the minimum coverage that will not generate an error)
@@ -233,6 +236,7 @@ ps_morph_Inv_phylo <- phyloseq(otu_table(ps_morph_Inv, taxa_are_rows = TRUE))#,
 
 ##Calculate the Coverage
 Coverage_morph_Inv <- phyloseq_coverage(physeq = ps_morph_Inv_phylo)
+Coverage_morph_Inv_correct_singletons <- phyloseq_coverage(physeq = ps_morph_Inv_phylo, correct_singletons = T)
 
 ##Rarefy the data based on a minimum coverage using the function phyloseq_coverage_raref,
 ps_morph_rarefied_Inv <- phyloseq_coverage_raref(physeq = ps_morph_Inv_phylo, 
@@ -346,6 +350,251 @@ p_all <- plot_richness(ps_all_phylo, x='Zone', measures=c("Observed", "Shannon")
 p_all$layers <- p_all$layers[-1]
 p_all$data$Zone <- factor(p_all$data$Zone, levels=unique(smpl_eDNA_Fish$Zone))
 p_all
+
+##Statistical analysis - Fish
+#Alpha diversity
+library(car)
+library(DescTools)
+library(lsr)
+DNA_species <- smpl_all
+DNA_species$NumOfSp <- colSums(ps_all>0)
+DNA_species_Fish <- DNA_species[which(DNA_species$Organism == "Fish"),]
+DNA_species_Demersal <- DNA_species_Fish[which(DNA_species_Fish$Method %in% c("eDNA_Demersal", "Morphology_Demersal")),]
+DNA_species_AllFish <- DNA_species_Fish[which(DNA_species_Fish$Method %in% c("eDNA_All", "Morphology_All")),]
+NumOfSp_AllFish <- as.data.frame(cbind(DNA_species_AllFish$NumOfSp,
+                                       DNA_species_AllFish$Zone,
+                                       DNA_species_AllFish$Method))
+NumOfSp_Demersal <- as.data.frame(cbind(DNA_species_Demersal$NumOfSp,
+                                       DNA_species_Demersal$Zone,
+                                       DNA_species_Demersal$Method))
+
+#model_DNA <- lm(NumOfSp ~ Zone, data=DNA_species, family=poisson)
+#model_DNA_AllFish <- glm(NumOfSp ~ Zone*Method, data=DNA_species_AllFish, family=poisson)
+#model_DNA_Demersal <- glm(NumOfSp ~ Zone*Method, data=DNA_species_Demersal, family=poisson)
+
+model_DNA_AllFish <- lm(NumOfSp ~ Zone*Method, data=DNA_species_AllFish)
+model_DNA_Demersal <- lm(NumOfSp ~ Zone*Method, data=DNA_species_Demersal) 
+
+anova_DNA_AllFish <- Anova(model_DNA_AllFish, type=2)# SIGN for station , not sign for biological replicates
+summary(model_DNA_AllFish)
+anova_DNA_AllFish
+anova_DNA_Demersal <- Anova(model_DNA_Demersal, type=2)# SIGN for station , not sign for biological replicates
+summary(model_DNA_Demersal)
+anova_DNA_Demersal
+PostHocTest(aov(NumOfSp ~ Zone*Method, data=DNA_species_AllFish), method = "hsd")
+PostHocTest(aov(NumOfSp ~ Zone*Method, data=DNA_species_Demersal), method = "hsd")
+
+es <- etaSquared(model_DNA_AllFish, type=2, anova=TRUE)
+es
+sum(es[,"eta.sq"]) # 0.95539
+
+es <- etaSquared(model_DNA_Demersal, type=2, anova=TRUE)
+es
+sum(es[,"eta.sq"]) # 0.872
+
+library(lsmeans)
+library(multcomp)
+cld(lsmeans(model_DNA_AllFish, ~ Zone*Method), Letters=letters)
+cld(lsmeans(model_DNA_Demersal, ~ Zone*Method), Letters=letters)
+#elk station in ander groep!
+#lstrends(model_DNA, "Station", var="NR") #lstrends: moet continue zijn + volgorde belangrijk => DUS HIER NIET GEBRUIKEN
+#DNA_species_2$NR <- as.numeric(as.factor(DNA_species_2$Biologic_replicate))
+
+#check assumptions
+#homogeneity
+plot(model_DNA_AllFish, 1)
+plot(model_DNA_Demersal, 1)
+#leveneTest(NumOfSp ~ Station, data=DNA_species) # SIGNIFICANT (but only on edges)
+leveneTest(NumOfSp ~ Zone*Method, data=DNA_species_AllFish)
+leveneTest(NumOfSp ~ Zone*Method, data=DNA_species_Demersal)
+
+#normality
+resid_all <- residuals(model_DNA_AllFish)  # pull the residuals
+hist(resid_all)
+qqnorm(resid_all) 
+resid_demersal <- residuals(model_DNA_Demersal)  # pull the residuals
+hist(resid_demersal)
+qqnorm(resid_demersal) 
+plot(model_DNA_AllFish, 2)
+plot(model_DNA_Demersal, 2)
+ggqqplot(DNA_species$NumOfSp)
+shapiro.test(resid_all)
+shapiro.test(resid_demersal)
+
+library(MASS)
+boxcox(model_DNA_AllFish)
+bc<-boxcox(model_DNA_AllFish)
+bc$x[which(bc$y==max(bc$y))] #0.4646465
+
+boxcox(model_DNA_Demersal)
+bc<-boxcox(model_DNA_Demersal)
+bc$x[which(bc$y==max(bc$y))] #0.989899
+
+#Shannon diversity
+DNA_Shannon <- smpl_all
+DNA_Shannon$NumOfSp <- (diversity(t(ps_all), "shannon"))
+DNA_Shannon_Fish <- DNA_Shannon[which(DNA_Shannon$Organism == "Fish"),]
+DNA_Shannon_Demersal <- DNA_Shannon_Fish[which(DNA_Shannon_Fish$Method %in% c("eDNA_Demersal", "Morphology_Demersal")),]
+DNA_Shannon_AllFish <- DNA_Shannon_Fish[which(DNA_Shannon_Fish$Method %in% c("eDNA_All", "Morphology_All")),]
+NumOfSp_AllFish <- as.data.frame(cbind(DNA_Shannon_AllFish$NumOfSp,
+                                       DNA_Shannon_AllFish$Zone,
+                                       DNA_Shannon_AllFish$Method))
+NumOfSp_Demersal <- as.data.frame(cbind(DNA_Shannon_Demersal$NumOfSp,
+                                        DNA_Shannon_Demersal$Zone,
+                                        DNA_Shannon_Demersal$Method))
+
+#model_Shannon_AllFish <- glm(NumOfSp ~ Zone*Method, data=DNA_Shannon_AllFish, family=poisson)
+#model_Shannon_Demersal <- glm(NumOfSp ~ Zone*Method, data=DNA_Shannon_Demersal, family=poisson) 
+
+model_Shannon_AllFish <- glm(NumOfSp ~ Zone*Method, data=DNA_Shannon_AllFish)
+model_Shannon_Demersal <- glm(NumOfSp ~ Zone*Method, data=DNA_Shannon_Demersal) 
+
+anova_Shannon_AllFish <- Anova(model_Shannon_AllFish, type=2)# SIGN for station , not sign for biological replicates
+summary(model_Shannon_AllFish)
+anova_Shannon_AllFish
+anova_Shannon_Demersal <- Anova(model_Shannon_Demersal, type=2)# SIGN for station , not sign for biological replicates
+summary(model_Shannon_Demersal)
+anova_Shannon_Demersal
+PostHocTest(aov(NumOfSp ~ Zone*Method, data=DNA_Shannon_AllFish), method = "hsd")
+PostHocTest(aov(NumOfSp ~ Zone*Method, data=DNA_Shannon_Demersal), method = "hsd")
+
+es <- etaSquared(model_Shannon_AllFish, type=2, anova=TRUE)
+es
+sum(es[,"eta.sq"]) # 0.9606682
+
+es <- etaSquared(model_Shannon_Demersal, type=2, anova=TRUE)
+es
+sum(es[,"eta.sq"]) # 1.041221
+
+cld(lsmeans(model_Shannon_AllFish, ~ Zone*Method), Letters=letters)
+cld(lsmeans(model_Shannon_Demersal, ~ Zone*Method), Letters=letters)
+
+#check assumptions
+#homogeneity
+plot(model_Shannon_AllFish, 1)
+plot(model_Shannon_Demersal, 1)
+leveneTest(NumOfSp ~ Zone*Method, data=DNA_Shannon_AllFish)
+leveneTest(NumOfSp ~ Zone*Method, data=DNA_Shannon_Demersal)
+
+#normality
+resid_all <- residuals(model_Shannon_AllFish)  # pull the residuals
+hist(resid_all)
+qqnorm(resid_all) 
+resid_demersal <- residuals(model_Shannon_Demersal)  # pull the residuals
+hist(resid_demersal)
+qqnorm(resid_demersal) 
+plot(model_Shannon_AllFish, 2)
+plot(model_Shannon_Demersal, 2)
+ggqqplot(DNA_species$NumOfSp)
+shapiro.test(resid_all)
+shapiro.test(resid_demersal)
+
+##Statistical analysis - Invertebrates
+#Alpha diversity
+DNA_species_Inv <- DNA_species[which(DNA_species$Organism == "Invertebrates"),]
+
+model_DNA_Inv <- lm(NumOfSp ~ Zone*Method, data=DNA_species_Inv) 
+model_DNA2_Inv <- glm(NumOfSp ~ Zone*Method, data=DNA_species_Inv, family = poisson) 
+anova_DNA_Inv <- Anova(model_DNA_Inv, type=2)# SIGN for station , not sign for biological replicates
+summary(model_DNA_Inv)
+anova_DNA_Inv
+anova_DNA2_Inv <- Anova(model_DNA2_Inv, type=2)# SIGN for station , not sign for biological replicates
+summary(model_DNA2_Inv)
+anova_DNA2_Inv
+PostHocTest(aov(NumOfSp ~ Zone*Method, data=DNA_species_Inv), method = "hsd")
+
+es <- etaSquared(model_DNA_Inv, type=2, anova=TRUE)
+es
+sum(es[,"eta.sq"]) # 1.000002
+
+es <- etaSquared(model_DNA2_Inv, type=2, anova=TRUE)
+es
+sum(es[,"eta.sq"]) # 0.8668192
+
+cld(lsmeans(model_DNA_Inv, ~ Zone*Method), Letters=letters)
+cld(lsmeans(model_DNA2_Inv, ~ Zone*Method), Letters=letters)
+
+#elk station in ander groep!
+#lstrends(model_DNA, "Station", var="NR") #lstrends: moet continue zijn + volgorde belangrijk => DUS HIER NIET GEBRUIKEN
+#DNA_species_2$NR <- as.numeric(as.factor(DNA_species_2$Biologic_replicate))
+
+#check assumptions
+#homogeneity
+plot(model_DNA_Inv, 1)
+plot(model_DNA2_Inv, 1)
+leveneTest(NumOfSp ~ Zone*Method, data=DNA_species_Inv)
+
+#normality
+resid <- residuals(model_DNA_Inv)  # pull the residuals
+resid2 <- residuals(model_DNA2_Inv)  # pull the residuals
+hist(resid)
+hist(resid2)
+qqnorm(resid)
+qqnorm(resid2) 
+plot(model_DNA_Inv, 2)
+plot(model_DNA2_Inv, 2)
+ggqqplot(DNA_species_Inv$NumOfSp)
+shapiro.test(resid)
+shapiro.test(resid2)
+
+boxcox(model_DNA_Inv)
+bc<-boxcox(model_DNA_Inv)
+bc$x[which(bc$y==max(bc$y))] #0.6666667
+
+boxcox(model_DNA2_Inv)
+bc<-boxcox(model_DNA2_Inv)
+bc$x[which(bc$y==max(bc$y))] #0.6666667
+
+#Shannon diversity
+DNA_Shannon_Inv <- DNA_Shannon[which(DNA_Shannon$Organism == "Invertebrates"),]
+
+model_Shannon_Inv <- lm(NumOfSp ~ Zone*Method, data=DNA_Shannon_Inv)
+model_Shannon2_Inv <- glm(NumOfSp ~ Zone*Method, data=DNA_Shannon_Inv, family = poisson)
+anova_Shannon_Inv <- Anova(model_Shannon_Inv, type=2)# SIGN for station , not sign for biological replicates
+summary(model_Shannon_Inv)
+anova_Shannon_Inv
+anova_Shannon2_Inv <- Anova(model_Shannon2_Inv, type=2)# SIGN for station , not sign for biological replicates
+summary(model_Shannon2_Inv)
+anova_Shannon2_Inv
+PostHocTest(aov(NumOfSp ~ Zone*Method, data=DNA_Shannon_Inv), method = "hsd")
+
+es <- etaSquared(model_Shannon_Inv, type=2, anova=TRUE)
+es
+sum(es[,"eta.sq"]) #1.00072 
+
+es <- etaSquared(model_Shannon2_Inv, type=2, anova=TRUE)
+es
+sum(es[,"eta.sq"]) #0.7441658
+
+cld(lsmeans(model_Shannon_Inv, ~ Zone*Method), Letters=letters)
+cld(lsmeans(model_Shannon2_Inv, ~ Zone*Method), Letters=letters)
+
+#check assumptions
+#homogeneity
+plot(model_Shannon_Inv, 1)
+plot(model_Shannon2_Inv, 1)
+leveneTest(NumOfSp ~ Zone*Method, data=DNA_Shannon_Inv)
+
+#normality
+resid_Shannon_Inv <- residuals(model_Shannon_Inv)  # pull the residuals
+resid_Shannon2_Inv <- residuals(model_Shannon2_Inv)  # pull the residuals
+hist(resid_Shannon_Inv)
+hist(resid_Shannon2_Inv)
+qqnorm(resid_Shannon_Inv) 
+qqnorm(resid_Shannon2_Inv) 
+plot(model_Shannon_Inv, 2)
+plot(model_Shannon2_Inv, 2)
+ggqqplot(DNA_Shannon$Shannon)
+shapiro.test(resid_Shannon_Inv)
+shapiro.test(resid_Shannon2_Inv)
+
+boxcox(model_Shannon_Inv)
+bc<-boxcox(model_Shannon_Inv)
+bc$x[which(bc$y==max(bc$y))]
+
+boxcox(model_Shannon2_Inv)
+bc<-boxcox(model_Shannon2_Inv)
+bc$x[which(bc$y==max(bc$y))]
 
 
 
